Added more tests and fixed a bug in regret calculation

fedmoe/client_manager.py
@@ -98,8 +98,8 @@ class ClientManager:
         return torch.cat(round_predictions, dim=1)
 
     def get_predictions_with_beta(self, round: int, betas: torch.Tensor) -> torch.Tensor:
-        # beta shape is Nd_z x d_y ---> to N x d_z x d_y
-        assert betas.shape == (self.num_clients, self.z_dim, self.y_dim)
+        # beta shape is Nd_z x 1 ---> to N x d_z x 1
+        assert betas.shape == (self.num_clients, self.z_dim, 1)
         new_round_predictions = []
         i = 0
         for client in self.clients:

fedmoe/tests/test_nash_objective.py
@@ -1,7 +1,9 @@
+import torch
+
 from fedmoe.game import RfnGame
 from fedmoe.metrics import RMSEMetric
 from fedmoe.server import Server
-from fedmoe.tests.game_utils import compute_game_regret_objective
+from fedmoe.tests.test_game_utils import compute_game_regret_objective
 from fedmoe.tests.utils import get_client_manager_dy_dx_1
 
 
@@ -48,7 +50,7 @@ def test_nash_game_objective_sync_step() -> None:
                     [game_server.server_outputs[i]],
                     client_manager.gamma,
                     client_manager.alpha,
-                    0,
+                    1,
                 )
                 game_no_Y_regret = compute_game_regret_objective(
                     [client.state.get_beta_t(i - 1)],
@@ -56,7 +58,7 @@ def test_nash_game_objective_sync_step() -> None:
                     [game_server_no_Y.server_outputs[i]],
                     client_manager.gamma,
                     client_manager.alpha,
-                    0,
+                    1,
                 )
                 no_game_regret = compute_game_regret_objective(
                     [client.state.get_beta_t(i - 1)],
@@ -64,7 +66,7 @@ def test_nash_game_objective_sync_step() -> None:
                     [simple_server.server_outputs[i]],
                     client_manager.gamma,
                     client_manager.alpha,
-                    0,
+                    1,
                 )
                 assert (
                     game_no_Y_regret < no_game_regret
@@ -106,26 +108,102 @@ def test_nash_game_objective_accumulative() -> None:
             for client_id in range(0, client_manager.num_clients):
                 client = client_manager.clients[client_id]
 
-                game_no_Y_regret = compute_game_regret_objective(
-                    [client.state.get_beta_t(j) for j in range(i - sync_freq, i)],
-                    [target[j] for j in range(i - sync_freq + 1, i + 1)],
-                    [game_server_no_Y.server_outputs[j] for j in range(i - sync_freq + 1, i + 1)],
+                game_regret = compute_game_regret_objective(
+                    [client.state.get_beta_t(i - j - 1) for j in range(0, game.sync_freq)],
+                    [target[i - j] for j in range(0, game.sync_freq)],
+                    [game_server_no_Y.server_outputs[i - j] for j in range(0, game.sync_freq)],
                     client_manager.gamma,
                     client_manager.alpha,
-                    backward_time_length=(sync_freq - 1),
+                    backward_time_length=sync_freq,
                 )
-                game_sum_regret += game_no_Y_regret
+                game_sum_regret += int(game_regret)
 
                 no_game_regret = compute_game_regret_objective(
-                    [client.state.get_beta_t(j) for j in range(i - sync_freq, i)],
-                    [target[j] for j in range(i - sync_freq + 1, i + 1)],
-                    [simple_server.server_outputs[j] for j in range(i - sync_freq + 1, i + 1)],
+                    [client.state.get_beta_t(i - j - 1) for j in range(0, game.sync_freq)],
+                    [target[i - j] for j in range(0, game.sync_freq)],
+                    [simple_server.server_outputs[i - j] for j in range(0, game.sync_freq)],
                     client_manager.gamma,
                     client_manager.alpha,
-                    backward_time_length=(sync_freq - 1),
+                    backward_time_length=sync_freq,
                 )
-                simple_sum_regret += no_game_regret
+                simple_sum_regret += int(no_game_regret)
 
     assert (
         game_sum_regret < simple_sum_regret
-    ), f" Accumulative no_game_regret: {game_sum_regret}, game_no_Y_regret: {simple_sum_regret}"
+    ), f" Accumulative game_regret: {game_sum_regret}, no_game_regret: {simple_sum_regret}"
+
+
+def test_nash_beta() -> None:
+    seed = 2024
+    torch.manual_seed(seed)
+    data_length = 20
+    sync_freq = 3
+    client_manager = get_client_manager_dy_dx_1(
+        alpha=0.1, gamma=0.1, z_dim=3, data_length=data_length, sync_freq=sync_freq
+    )
+    target = client_manager.common_target_sequence
+    game = RfnGame(
+        client_manager.clients,
+        sync_freq=sync_freq,
+        z_dim=3,
+    )
+
+    client_manager.clients = client_manager.initiate_clients()
+    game_server = Server(sync_freq, client_manager, game, metrics=[RMSEMetric("RSME")], kappa=2.0, eta=1)
+    _ = game_server.fit(data_length - 1, have_sync=True, update_last_Y_sync=False)
+    clients = client_manager.clients
+    # We have to test it for both clients because game works on the mixture weights (so we need all clients).
+    for i in range(0, len(target)):
+        if i % game.sync_freq == 0 and i > 0:
+            # Check if the Nash beta is minimal based on the regret function
+            # Get the Nash beta
+            opt_regret_0 = compute_game_regret_objective(
+                [clients[0].state.get_beta_t(i - 1)],
+                [target[i]],
+                [game_server.server_outputs[i]],
+                client_manager.gamma,
+                client_manager.alpha,
+                1,
+            )
+            opt_regret_1 = compute_game_regret_objective(
+                [clients[1].state.get_beta_t(i - 1)],
+                [target[i]],
+                [game_server.server_outputs[i]],
+                client_manager.gamma,
+                client_manager.alpha,
+                1,
+            )
+            opt_regret = opt_regret_0 + opt_regret_1
+            # test whether any randomly drawn betas are better
+            for j in range(100000):
+                # z_dim = 3
+                # beta shape is N x d_z x d_y
+                test_betas = torch.randn((2, 3, 1)).double()
+                # New predictions shape is d_y x 1
+                new_preds = client_manager.get_predictions_with_beta(i, test_betas)
+                # Nash betas are replaced with random beta in each client
+                # Compute new server output based on old mixture weights
+                new_server_output = torch.matmul(new_preds.double(), game_server.mixture_weights[i].double())
+                test_regret_0 = compute_game_regret_objective(
+                    [test_betas[0]],
+                    [target[i]],
+                    [new_server_output],
+                    client_manager.gamma,
+                    client_manager.alpha,
+                    1,
+                )
+                test_regret_1 = compute_game_regret_objective(
+                    [test_betas[1]],
+                    [target[i]],
+                    [new_server_output],
+                    client_manager.gamma,
+                    client_manager.alpha,
+                    1,
+                )
+
+                test_regret = test_regret_1 + test_regret_0
+                assert (
+                    test_regret > opt_regret
+                ), f" opt regret: {opt_regret}, test regret: {test_regret}, test_beta: {test_betas},\
+                      target: {target[i]}, game pred: {game_server.server_outputs[i]},\
+                            random pred: {new_server_output}"