e916566582c626c42014e376dcfc5bd0628ce266
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-02-15 15:20:33 -0500

    Some improvements to the visualization scripts, adding in the ability to visualize an overlay of the game and non-game predictions

--- fedmoe/utils/visualize_overlaid_server_predictions.py
+++ fedmoe/utils/visualize_overlaid_server_predictions.py
@@ -16,49 +16,43 @@ def visualize_relative_server_squared_errors(
     render_line: bool = False,
 ) -> None:
 
-    total_time_steps = target_matrix.shape[0]
-
-    assert game_server_predictions.shape == (total_time_steps, target_matrix.shape[1]), {
-        f"Error:server output matrix has a shape {game_server_predictions.shape},\
-            but it should be{(total_time_steps, target_matrix.shape[1])}"
-    }
-
-    game_squared_error = torch.round((game_server_predictions - target_matrix) ** 2, decimals=8)
-    non_game_squared_error = torch.round((non_game_server_predictions - target_matrix) ** 2, decimals=8)
-
     plt.rcParams["figure.figsize"] = [10, 3]
     ax = plt.figure().gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
-    # Plot server's prediction squared errors relative to one another
+    # Plot target y
+    for i in range(target_matrix.shape[1]):
+        plt.plot(time_axis, target_matrix[:, i], label=f"Target: $y_{i+1}$", linestyle="-", linewidth=2.5)
+
+    # Plot server's prediction
     for i in range(game_server_predictions.shape[1]):
-        relative_error = non_game_squared_error[:, i] / game_squared_error[:, i]
-        plt.scatter(
+        plt.plot(
             time_axis,
-            relative_error.detach().numpy(),
-            label=f"$\\frac{{(\\hat{{Y}}^n_{i+1} - y_{i+1})^2}}{{(\\hat{{Y}}^g_{i+1} - y_{i+1})^2}}$",
+            game_server_predictions[:, i].detach().numpy(),
+            label=f"Nash Server $\\hat{{Y}}_{i+1}$",
+            linestyle=":",
+            linewidth=2.5,
         )
 
-    ax.set_yscale("log")
-
-    plt.plot(
-        time_axis,
-        torch.ones_like(time_axis),
-        linestyle="--",
-        linewidth=2.5,
-        color="red",
-    )
+    for i in range(non_game_server_predictions.shape[1]):
+        plt.plot(
+            time_axis,
+            non_game_server_predictions[:, i].detach().numpy(),
+            label=f"Non-Nash Server $\\hat{{Y}}_{i+1}$",
+            linestyle=":",
+            linewidth=2.5,
+        )
 
     title_font = {"family": "helvetica", "weight": "bold", "size": 20}
     axis_font = {"family": "helvetica", "weight": "bold", "size": 18}
     plt.xticks(fontname="helvetica", fontsize=14, fontweight="bold")
     plt.yticks(fontname="helvetica", fontsize=14, fontweight="bold")
     plt.xlabel("Time Step", fontdict=axis_font)
-    plt.ylabel("Ratio of Squared Errors", fontdict=axis_font)
-    plt.title("Ratio Squared Errors Without and With Nash Game", fontdict=title_font)
+    plt.ylabel("Time-Series Values", fontdict=axis_font)
+    plt.title(f"Server Predictions vs. Target", fontdict=title_font)
 
-    plt.legend(prop={"family": "helvetica", "weight": "bold", "size": 22}, loc="lower right", labelspacing=0)
-    plt.tight_layout(pad=0.5)
+    plt.legend(prop={"family": "helvetica", "weight": "bold", "size": 12}, labelspacing=0)
+    plt.tight_layout(pad=1)
 
     plt.savefig(plot_path)
 
@@ -118,6 +112,6 @@ if __name__ == "__main__":
         game_target,
         game_server_predictions,
         non_game_server_predictions,
-        os.path.join(output_dir, "relative_squared_server_errors.png"),
+        os.path.join(output_dir, "overlaid_server_predictions.png"),
         render_line=True,
     )
